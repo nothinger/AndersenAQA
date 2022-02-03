622eadd7b84e9236e11850604b13c19a3e20b695
nothinger <bradunovandreja@gmail.com>
2022-02-03 23:06:07 +0300

    Adding Some Test on RestAssured

--- src/test/java/RestAssuredDemoTests.java
+++ src/test/java/RestAssuredDemoTests.java
@@ -1,6 +1,10 @@
 import io.restassured.response.Response;
+import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -21,6 +25,95 @@ public class RestAssuredDemoTests {
     @Test
     public void testDemoExtras () {
         baseURI = "https://reqres.in/api";
-        given().get("/users?page=2").then().statusCode(200).body("data[1].id", equalTo(8)).log().all();
+        given().get("/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("data[1].id", equalTo(8))
+                .log()
+                .all();
+    }
+
+    @Test
+    public void testDemoGetRequestExample () {
+        baseURI = "https://reqres.in/api";
+        given().get("users/2")
+                .then()
+                .statusCode(200)
+                .body("data.first_name", equalTo("Janet"))
+                .log()
+                .all();
+    }
+
+    @Test
+    public void testDemoPostRequestExample () {
+        baseURI = "https://reqres.in/api";
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        JSONObject request = new JSONObject(map);
+        request.put("name", "Andrew");
+        request.put("job","AQA");
+
+        given()
+                .header("content-type","application/json")
+                .body(request.toJSONString())
+                .when()
+                .post("api/users")
+                .then()
+                .statusCode(201)
+                .log()
+                .all();
+    }
+
+    @Test
+    public void testDemoPutRequestExample () {
+        baseURI = "https://reqres.in/api";
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        JSONObject request = new JSONObject(map);
+        request.put("name", "Andrew");
+        request.put("job","AQA");
+
+        given()
+                .header("content-type","application/json")
+                .body(request.toJSONString())
+                .when()
+                .put("/api/users/2")
+                .then()
+                .statusCode(200)
+                .log()
+                .all();
+    }
+
+    @Test
+    public void testDemoPatchRequestExample () {
+        baseURI = "https://reqres.in/api";
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        JSONObject request = new JSONObject(map);
+        request.put("name", "Andrew");
+        request.put("job","AQA");
+
+        given()
+                .header("content-type","application/json")
+                .body(request.toJSONString())
+                .when()
+                .patch("/api/users/2")
+                .then()
+                .statusCode(200)
+                .log()
+                .all();
+    }
+
+    @Test
+    public void testDemoDeleteRequestExample () {
+        baseURI = "https://reqres.in/api";
+
+        given()
+                .when()
+                .delete("/api/users/2")
+                .then()
+                .statusCode(204)
+                .log()
+                .all();
     }
 }
